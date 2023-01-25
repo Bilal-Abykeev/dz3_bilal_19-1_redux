562042df613f59f9d51baa3e002885dc4106d570
Bilal-Abykeev <113285404+Bilal-Abykeev@users.noreply.github.com>
2023-01-25 23:58:32 +0300

    dz3

---
+++ src/pages/mainPage/MainPage.js
@@ -0,0 +1,26 @@
+import {useDispatch,useSelector} from "react-redux"
+import Counter from "../../components/Counter"
+
+function MainPage(params) {
+
+    const dispatch = useDispatch()
+
+    const title = useSelector(state => state.title)
+
+    const changeText = () => {
+        dispatch({
+            type:"CHANGE_TEXT"
+        })
+    }
+
+    return(
+        <>
+        <h1>{title}</h1>
+        <button onClick={changeText}>text</button>
+        <div>
+            <Counter/>
+        </div>
+        </>
+    )
+}
+export default MainPage;
